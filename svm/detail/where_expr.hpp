8306af9acca7887dfbc5b6e197aab98a27485786
switchblade <terekhin.mikhail.01@gmail.com>
2023-01-08 02:00:39 -0800

    Renamed simd_access to native_access

--- svm/detail/where_expr.hpp
+++ svm/detail/where_expr.hpp
@@ -20,7 +20,7 @@ namespace svm
 	{
 		/* Clang-tidy was having issues with friended concept-constrained functions and types, so this is a workaround. */
 		template<typename>
-		struct storage_access;
+		struct native_access;
 
 		template<typename>
 		struct valid_mask : std::false_type {};
